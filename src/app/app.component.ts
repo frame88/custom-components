db7c1ba19a6a9593fba7baed612f309e639ca03d
frame88 <fleuzzi97@gmail.com>
2022-09-06 10:36:38 +0200

    creazione di un componente; importazione in app.component; dichiarazione in app.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div class="container-fluid d-flex justify-content-center mt-5">
+    <div class="container text-center">
+      siamo in app.component ma aggiungo <app-hello></app-hello>
+  
+    </div>
+
+  </div>
+    `,
+  styles: []
 })
 export class AppComponent {
   title = 'custom-components';
 }
+
+// Hello Components
+
+// nel file ts.lint.json in "component-selector" posso modificare 
+// le impostazioni di un componente, per esempio se non avesse prefisso "app"
---
+++ src/app/components/hello.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-hello',
+    template: '<h1>stringa di esempio</h1>'
+})
+export class HelloComponent {
+
+}
